b5be035d61d57ffda37669f440aa92cac3778d35
Cauã Carvalho <caua2006carvalho@gmail.com>
2025-02-18 11:02:13 +0000

    eat: Create a first POST route and iniciallize Swagger Docs

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,52 @@
-console.log('Hello World');
+import fastify from "fastify";
+import { fastifyCors }from '@fastify/cors'
+import {
+    validatorCompiler,
+    serializerCompiler,
+    ZodTypeProvider,
+    jsonSchemaTransform
+} from 'fastify-type-provider-zod'
+import z from "zod";
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+app.register(fastifyCors)
+
+app.register(fastifySwagger, {
+    openapi: {
+        info: {
+            title: "NLW Connect",
+            version: "0.0.1"
+        }
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi, {
+    routePrefix: "/docs",
+})
+
+app.post('/subscriptions',{
+    schema:{
+        body: z.object({
+            name: z.string(),
+            email: z.string().email()
+        }),
+        response: {
+            201: z.object({
+                name: z.string(),
+                email: z.string().email()
+            })
+        }
+    }
+}, async (request, reply)=>{
+    const { name, email } = request.body 
+
+    return reply.status(201).send({ name, email})
+})
+
+app.listen({port:3333}).then(()=>{
+    console.log('Http Server running: http://localhost:3333')
+})
