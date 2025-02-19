1ec5f14054b55d59d2fe59340f18cadb2b7c0deb
caua2006 <caua2006carvalho@gmail.com>
2025-02-18 22:04:08 -0300

    Feat: connect with docker and start Drizzle ORM

--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,37 @@
-import fastify from "fastify";
-import { fastifyCors }from '@fastify/cors'
-import {
-    validatorCompiler,
-    serializerCompiler,
-    ZodTypeProvider,
-    jsonSchemaTransform
-} from 'fastify-type-provider-zod'
-import z from "zod";
+import { fastifyCors } from '@fastify/cors'
 import { fastifySwagger } from '@fastify/swagger'
 import { fastifySwaggerUi } from '@fastify/swagger-ui'
-const app = fastify().withTypeProvider<ZodTypeProvider>();
+import fastify from 'fastify'
+import {
+  type ZodTypeProvider,
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import z from 'zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
+const app = fastify().withTypeProvider<ZodTypeProvider>()
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 app.register(fastifyCors)
 
 app.register(fastifySwagger, {
-    openapi: {
-        info: {
-            title: "NLW Connect",
-            version: "0.0.1"
-        }
+  openapi: {
+    info: {
+      title: 'NLW Connect',
+      version: '0.0.1',
     },
-    transform: jsonSchemaTransform
+  },
+  transform: jsonSchemaTransform,
 })
 
 app.register(fastifySwaggerUi, {
-    routePrefix: "/docs",
+  routePrefix: '/docs',
 })
 
-app.post('/subscriptions',{
-    schema:{
-        body: z.object({
-            name: z.string(),
-            email: z.string().email()
-        }),
-        response: {
-            201: z.object({
-                name: z.string(),
-                email: z.string().email()
-            })
-        }
-    }
-}, async (request, reply)=>{
-    const { name, email } = request.body 
+app.register(subscribeToEventRoute)
 
-    return reply.status(201).send({ name, email})
-})
-
-app.listen({port:3333}).then(()=>{
-    console.log('Http Server running: http://localhost:3333')
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Http Server running: http://localhost:3333')
 })
